2b31a497c23f29fb40d95a39df742607e987181f
Aaron Moline <aaron.moline@gmail.com>
2018-01-10 19:06:12 -0600

    adding a custom pipe converter and a nested componet  (star component) that allows taking input

---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnChanges} from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+  starWidth: number = 4;
+  @Input() rating: number;
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 86 / 5;
+  }
+}
